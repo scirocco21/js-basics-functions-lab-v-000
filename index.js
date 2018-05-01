d7277a83e38c90d9d844b9f6dd94909497b34c8d
Seb Gertz <sebgertz@gmail.com>
2018-05-01 19:25:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+  return block >= 42 ? block - 42 : 42 - block;
+}
+
+function distanceFromHqInFeet(block){
+  return distanceFromHqInBlocks(block) * 264;
+}
+
+function distanceTravelledInFeet(origin, destination) {
+
+}
+
+function blocksTravelled(origin, destination) {
+  return destination > origin ? destination - origin : origin - destination;
+}
